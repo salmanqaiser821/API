be444012c53c251c75fe7d0999838f62b59fc115
Salman Qaisar <71202136+salmanqaiser821@users.noreply.github.com>
2024-04-02 14:12:28 +0500

    API
    Created Api with asp.net web api

---
+++ DemoAPI/Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using DemoAPI.Data;
+using DemoAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private ApplicationDbContext _db;
+        public StudentsController(ApplicationDbContext context)
+        {
+            _db = context;
+                
+        }
+        [HttpGet]
+        public List<StudentEntity> GetAllStudents()
+        {
+            return _db.StudentRegister.ToList();
+        }
+        [HttpGet("GetStudentsById")]
+        public ActionResult<StudentEntity> GetStudentDetails(Int32 Id) 
+        {
+            if(Id == 0 )
+            {
+                return BadRequest();
+            }
+
+
+            var StudentDetails = _db.StudentRegister.FirstOrDefault(x => x.Id == Id);
+
+            if (StudentDetails == null)
+            {
+                return NotFound();
+            }
+
+
+            return StudentDetails;
+
+        }
+        [HttpPost]
+        public ActionResult<StudentEntity> AddStudent([FromBody]StudentEntity StudentDetails) 
+        {
+        if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+        _db.StudentRegister.Add(StudentDetails);
+        _db.SaveChanges();
+        return Ok(StudentDetails);
+        }
+        [HttpPost("UpdateStudentDetails")]
+        public ActionResult<StudentEntity> UpdateStudent(Int32 Id, [FromBody] StudentEntity studendetails)
+        {
+            if (studendetails == null)
+            {
+                return BadRequest();       
+            }       
+            var StudentDetails = _db.StudentRegister.FirstOrDefault(x => x.Id == Id);
+            if(StudentDetails == null)
+            {
+                return NotFound();
+            }
+
+            StudentDetails.Name= StudentDetails.Name;
+            StudentDetails.Age = StudentDetails.Age;
+            StudentDetails.Standard= StudentDetails.Standard;
+            StudentDetails.EmailAddress= StudentDetails.EmailAddress;
+            _db.SaveChanges();
+            return Ok(StudentDetails);
+        }
+    }
+}
---
+++ DemoAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DemoAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<StudentEntity> StudentRegister { get; set; }
+    }
+}
